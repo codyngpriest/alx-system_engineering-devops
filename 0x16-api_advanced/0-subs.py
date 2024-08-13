36d31e36228efd60f5f43a16dce5b6060eeb132a
codyngpriest <vilhopriestly@gmail.com>
2024-08-13 06:37:21 +0000

    implement 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Write a Python script that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
+"""
+import json
+import requests
+import sys
+
+headers = {
+    'User-Agent': 'My User Agent 1.0'
+}
+
+
+def number_of_subscribers(subreddit):
+    """function that returns the number of subscribers"""
+    try:
+        url = 'https://www.reddit.com/r/'
+        response = requests.get(url + subreddit + "/about.json",
+                                headers=headers, allow_redirects=False)
+        return response.json()['data']['subscribers']
+    except:
+        return 0
